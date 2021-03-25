6c5e047b15783f7e7ca243c04e70f9e68c6ba197
Abernabern1 <abernabern1@gmail.com>
2021-03-25 22:41:47 +0200

    Made validation for SignUp command

--- manager/src/Model/User/UseCase/SignUp/Command.php
+++ manager/src/Model/User/UseCase/SignUp/Command.php
@@ -2,10 +2,32 @@
 
 namespace App\Model\User\UseCase\SignUp;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
     public $email;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3)
+     */
     public $login;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6)
+     */
     public $password;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6)
+     */
     public $passwordRepeat;
 }
