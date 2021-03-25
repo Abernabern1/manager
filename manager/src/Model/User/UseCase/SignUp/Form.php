5c040616d7a11dc26ff1b9b3429afdcc29aa8fa7
Abernabern1 <abernabern1@gmail.com>
2021-03-25 22:55:48 +0200

    Added SignUp form and User migration

---
+++ manager/src/Model/User/UseCase/SignUp/Form.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model\User\UseCase\SignUp;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('login', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('passwordRepeat', PasswordType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+}
