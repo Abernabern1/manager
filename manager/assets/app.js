dfe30f3af208b47314fe44cfe7e970f2c42ea254
Anmaner <Anmanerr@gmail.com>
2021-04-08 19:11:47 +0300

    Required core ui

--- manager/assets/app.js
+++ manager/assets/app.js
@@ -1,12 +1,7 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-
-// start the Stimulus application
 import './bootstrap';
+
+import '@coreui/coreui/dist/js/coreui.bundle';
+import '@coreui/chartjs/dist/js/coreui-chartjs.bundle';
+import '@coreui/icons/js/svgxuse';
+import '@coreui/utils/dist/coreui-utils';
